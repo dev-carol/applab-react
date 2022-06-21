dbd8787641773ddc77910715e752fb489784300d
carol santos <contato.caroline1999@gmail.com>
2022-06-21 10:12:44 -0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="App">
+       
+       <h1 className="text-5xl font-bold via-violet-400">Hello world!</h1>
+        
+    </div>
+  )
+}
+
+export default App
