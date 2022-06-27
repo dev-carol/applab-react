a0db94ad3043b354e25bd12bcadbfde2370e84aa
carol santos <contato.caroline1999@gmail.com>
2022-06-27 02:13:44 -0300

    feat: add date and format

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,4 +1,6 @@
 import { CheckCircle, Lock } from "phosphor-react";
+import {isPast, format} from 'date-fns'
+import ptBR from'date-fns/locale/pt-BR'
 
 interface LessonProps {
   title: string;
@@ -8,10 +10,13 @@ interface LessonProps {
 }
 export default function Lesson(props: LessonProps) {
 
-    const isLessonAvailable = false;
+    const isLessonAvailable = isPast(props.availableAt);
+    const availableDateFormatted = format(props.availableAt, "EEEE' • 'd' de 'MMMM' ' • ' 'k' 'h' 'mm",{
+        locale: ptBR
+    })
   return (
     <a href="#">
-      <span className="text-gray-300">{props.availableAt.toString()}</span>
+      <span className="text-gray-300">{availableDateFormatted}</span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center  justify-between">
