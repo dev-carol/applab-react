90737ef61facc900d07679dc0d90178653e7b964
carol santos <contato.caroline1999@gmail.com>
2022-06-28 11:19:48 -0300

    feat: created routes

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Link } from "react-router-dom";
+import Event from "../pages/Event";
 export function Router() {
   return(
     <Routes>
@@ -7,7 +8,7 @@ export function Router() {
         />
 
 <Route path="/event"
-        element={<h1>Event</h1>}
+        element={<Event/>}
         />
      </Routes>
   )
